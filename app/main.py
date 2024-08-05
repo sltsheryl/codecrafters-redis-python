4a41c46dc6749648fc2ea198acdffd6c82028b72
sltsheryl <sltsheryl@gmail.com>
2024-08-06 00:10:42 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,8 +10,12 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client, addr = server_socket.accept()
-    client.send(b"+PONG\r\n")
 
+    while True:
+        data = client.recv(1024)
+        if not data:
+            break
+        client.send("+PONG\r\n".encode())
 
 if __name__ == "__main__":
     main()
