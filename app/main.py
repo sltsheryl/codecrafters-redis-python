412a3ea71f435d6fe5605c0c331812c8ce5d374a
sltsheryl <sltsheryl@gmail.com>
2024-08-06 21:19:52 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,12 +2,17 @@ import asyncio
 import argparse
 from app.server import RedisServer
 
-async def main(port):
-    server = RedisServer(role='master', port=port)
+async def main(port, replicaof):
+    if replicaof:
+        master_host, master_port = replicaof.split(" ")
+        server = RedisServer(role='slave', port=port, master_host=master_host, master_port=int(master_port))
+    else:
+        server = RedisServer(port=port)
     await server.start()
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Redis Server")
     parser.add_argument("--port", type=int, default=6379, help="Port to run the server on")
+    parser.add_argument("--replicaof", type=str, help="Replicate data from master")
     args = parser.parse_args()
-    asyncio.run(main(args.port))
+    asyncio.run(main(args.port, args.replicaof))
