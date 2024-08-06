2d909f23cf31919047ca6c037fa78811be3d34d9
sltsheryl <sltsheryl@gmail.com>
2024-08-06 22:08:13 +0800

    codecrafters submit [skip ci]

--- app/server.py
+++ app/server.py
@@ -15,8 +15,26 @@ class RedisServer:
         self.replication_id = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb"
         self.offset = 0
 
-        if self.role == 'replica' and self.master_host and self.master_port:
+    async def start(self):
+        if self.role == 'slave':
+            # ping master
+            await self.ping_master()
+            # sync with master
             asyncio.create_task(self.sync_with_master())
+        server = await asyncio.start_server(self.handle_client, self.host, self.port)
+        async with server:
+            await server.serve_forever()
+        
+    
+    async def ping_master(self):
+        reader, writer = await asyncio.open_connection(self.master_host, self.master_port)
+        # PING command should be sent as a RESP Array
+        writer.write("*1\r\n$4\r\nPING\r\n".encode())
+        await writer.drain()
+        data = await reader.read(1024)
+        if data.decode() != "+PONG\r\n":
+            raise Exception("Failed to ping master")
+        writer.close()
 
     async def sync_with_master(self):
         # replicate data from master
@@ -54,8 +72,3 @@ class RedisServer:
     def set_replica(self, master_host, master_port):
         self.replicas.append((master_host, master_port))
         return "+OK\r\n"
-
-    async def start(self):
-        server = await asyncio.start_server(self.handle_client, self.host, self.port)
-        async with server:
-            await server.serve_forever()
